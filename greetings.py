b50395a5a44dfd135785b5671d722ef6af0320d9
KgasagoGH <kgasago.mash@gmail.com>
2024-03-07 10:32:03 +0200

    clear

--- greetings.py
+++ greetings.py
@@ -1,4 +1,4 @@
 name = input("What is your name? ")
-print(f"Goodbye, {name}!")
-name = input("What is your name? ")
 print(f"Hello, {name}!")
+name = input("What is your name? ")
+print(f"Goodbye, {name}!")
