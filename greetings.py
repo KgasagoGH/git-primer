a1fc0be8065a6eb812bb4e6dfd1d6b4298f9ff7b
Ntebatse Rachidi <ntebatserachidi@icloud.com>
2024-03-07 14:17:25 +0200

    review code

--- greetings.py
+++ greetings.py
@@ -1,6 +1,8 @@
-from fatetime import datetime
+from datetime import datetime
 
 name = input("What is your name")
 hour = datetime.now().time().hour
 greeting = "Good morning" if 4 <= hour < else "Good afternoon" if 12 <= hour < 16 else "Good night"
-print(f"{greeting}",{name}!)
+print(f"{greeting}",{name})
+
+ 
